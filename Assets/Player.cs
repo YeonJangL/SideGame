f75bd43b004056f31ff5abe2038d6793c05d7b76
YeonJangL <lej32552002@gmail.com>
2024-04-11 17:49:40 +0900

    2024 - 04 - 11 수업 진도

---
+++ Assets/Player.cs
@@ -0,0 +1,202 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Entity
+{
+    /*private Rigidbody2D rb;
+    private Animator anim;*/
+
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
+
+    [Header("Dash info")]
+    [SerializeField] private float dashSpeed;
+    [SerializeField] private float dashDuration;
+    private float dashTime;
+
+    [SerializeField] private float dashCooldown;
+    private float dashCooldownTimer;
+
+    [Header("Attack info")]
+    [SerializeField]private float comboTime = .3f;
+    private float comboTimeWindow;
+
+    private bool isAttacking;
+    private int comboCounter;
+
+    private float xInput;
+
+    /*private int facingDir = 1;
+    private bool facingRight = true;*/
+
+    // Entity.cs∑Œ ¿Ãµø
+    /*[Header("Collision info")]
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private LayerMask whatIsGround;*/
+    /*private bool isGrounded;*/
+
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    // Start is called before the first frame update
+    /*void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
+        *//*rb.velocity = new Vector2(5, rb.velocity.y);*//*
+    }*/
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        base.Update();
+
+        Movement();
+
+        CheckInput();
+
+        CollisionChecks();
+
+        dashTime -= Time.deltaTime;
+        dashCooldownTimer -= Time.deltaTime; 
+        comboTimeWindow -= Time.deltaTime;
+
+        FlipController();
+
+        AnimatorControllers();
+
+        /*Debug.Log(Input.GetAxisRaw("Horizontal"));*/
+
+        /*if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Jump");
+        }*/
+    }
+
+    public void AttackOver()
+    {
+        isAttacking = false;
+
+        comboCounter++;
+
+        if (comboCounter > 2)
+        {
+            comboCounter = 0;
+        }
+    }
+
+    /*private void CollisionChecks()
+    {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
+    }*/
+
+    private void CheckInput()
+    {
+        xInput = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            StartAttackEvent();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W))
+        {
+            Jump();
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            DashAbility();
+        }
+    }
+
+    private void StartAttackEvent()
+    {
+        if (!isGrounded)
+        {
+            return;
+        }
+
+        if (comboTimeWindow < 0)
+        {
+            comboCounter = 0;
+        }
+
+        isAttacking = true;
+        comboTimeWindow = comboTime;
+    }
+
+    private void DashAbility()
+    {
+        if (dashCooldownTimer < 0 && !isAttacking)
+        {
+            dashCooldownTimer = dashCooldown;
+            dashTime = dashDuration;
+        }
+    }
+
+    private void Movement()
+    {
+        if (isAttacking)
+        {
+            rb.velocity = new Vector2 (0, 0);
+        }
+
+        else if (dashTime > 0)
+        {
+            rb.velocity = new Vector2(facingDir * dashSpeed, 0);
+        }
+        else
+        {
+            rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
+        }
+    }
+
+    private void Jump()
+    {
+        if (isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+
+    private void AnimatorControllers()
+    {
+        bool isMoving = rb.velocity.x != 0;
+
+        anim.SetFloat("yVelocity", rb.velocity.y);
+
+        anim.SetBool("isMoving", isMoving);
+        anim.SetBool("isGrounded", isGrounded);
+        anim.SetBool("isDashing", dashTime > 0);
+        anim.SetBool("isAttacking", isAttacking);
+        anim.SetInteger("comboCounter", comboCounter);
+    }
+
+    /*private void Flip()
+    {
+        facingDir = facingDir * -1;
+        facingRight = !facingRight;
+        transform.Rotate(0, 180, 0);
+    }*/
+
+    private void FlipController()
+    {
+        if (rb.velocity.x > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (rb.velocity.x < 0 && facingRight)
+        {
+            Flip();
+        }
+    }
+
+    /*private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, new Vector3(transform.position.x, transform.position.y - groundCheckDistance));
+    }*/
+}
